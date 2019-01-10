a3ee4ac2c955a8af44dd0039603fdfbff60b1931
unknown <ravi189690@gmail.com>
2019-01-11 00:25:14 +0530

    Forms module

--- src/app/views/others/create-requirement/create-requirement.component.ts
+++ src/app/views/others/create-requirement/create-requirement.component.ts
@@ -9,15 +9,30 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CreateRequirementComponent implements OnInit {
 
   myForm: FormGroup;
+  paperTypes = ["Thermal", "Old One", "New paper", "Silica"];
+  paperSizes = ["22x28", "23x36", "24x34", "25x36", "28x40", "30x40", "36x46"];
+  gsms = [60, 70, 80, 90, 100, 170, 210, 240, 250, 300, 350, 400, 450];
+  UniqueId = Date.now();
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-  this.myForm = this.fb.group({
-    name: ['Benedict', Validators.required],
-    email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
-    message: ['', [Validators.required, Validators.minLength(15)]]
-  });
+    console.log(this.UniqueId);
+    this.myForm = this.fb.group({
+      jobId:[this.UniqueId],
+      extras:['', Validators.required],
+      printMode: ['', Validators.required],
+      gsm: ['', Validators.required],
+      plates: ['', Validators.required],
+      quality: ['', Validators.required],
+      vendor: ['', Validators.required],
+      paperType: ['', [Validators.required]],
+      paperSize: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(data) {
+    console.log(data.value);
   }
 
 }
